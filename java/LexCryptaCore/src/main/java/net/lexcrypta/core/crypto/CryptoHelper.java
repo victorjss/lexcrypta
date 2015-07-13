95cb426b5ee826931c6b607ee4c783e890d958ed
Víctor Suárez <victorjss@gmail.com>
2015-07-13 18:17:26 +0100

    Helper for crypto functions. * Initial key generator and cipher

---
+++ java/LexCryptaCore/src/main/java/net/lexcrypta/core/crypto/CryptoHelper.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2015 Víctor Suárez <victorjss@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package net.lexcrypta.core.crypto;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ *
+ * @author Víctor Suárez <victorjss@gmail.com>
+ */
+public class CryptoHelper {
+    KeyGenerator keyGenerator;
+    Cipher aesCipher;
+
+    public CryptoHelper() {
+        try {
+            keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128);
+            aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    
+    public SecretKey getNewKey() {
+        return keyGenerator.generateKey();
+    }
+    
+    public String convertKey(SecretKey key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+    
+    public SecretKey convertKey(String key) {
+        byte[] bytes = Base64.getDecoder().decode(key);
+        return new SecretKeySpec(bytes, 0, bytes.length, "AES");
+    }
+    
+        
+    
+}
