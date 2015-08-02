e6e96bacb11a320af67e29b7a0197fc7dac5d1b3
Víctor Suárez <victorjss@gmail.com>
2015-08-02 16:59:02 +0100

    Removed unused imports

--- java/LexCryptaDown/src/main/java/net/lexcrypta/web/down/servlet/DownServlet.java
+++ java/LexCryptaDown/src/main/java/net/lexcrypta/web/down/servlet/DownServlet.java
@@ -18,7 +18,6 @@
 package net.lexcrypta.web.down.servlet;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Base64;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
